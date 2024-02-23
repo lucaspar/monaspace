31452edbc0fd9858b403603a9ac534a7cb69924e
Lucas Parzianello <lucaspar@users.noreply.github.com>
2024-02-22 23:31:32 -0500

    unified installation scripts; more sensible defaults for linux

---
+++ util/install.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+set -e
+
+# if on linux, install fonts to ~/.local/share/fonts/Monaspace
+if [ "$(uname)" == "Linux" ]; then
+    FONTS_LOCATION="${XDG_DATA_HOME:-$HOME/.local/share}"/fonts
+# on mac, install fonts to ~/Library/Fonts/Monaspace
+elif [ "$(uname)" == "Darwin" ]; then
+    FONTS_LOCATION="${HOME}/Library/Fonts"
+else
+    echo "Could not determine operating system. Please install fonts manually."
+    exit 1
+fi
+MONASPACE_LOCATON="${FONTS_LOCATION}/Monaspace"
+SCRIPT_LOCATION="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+REPO_LOCATION="$(dirname "${SCRIPT_LOCATION}")"
+
+if [ ! -d "${FONTS_LOCATION}" ]; then
+    echo "No fonts directory found. Make sure '${FONTS_LOCATION}' exists."
+    exit 2
+fi
+
+if [ ! -d "${REPO_LOCATION}" ]; then
+    echo "Repository could not be found at '${REPO_LOCATION}'."
+    exit 2
+fi
+
+mkdir -p "${MONASPACE_LOCATON}"
+
+# remove all fonts from ~/.local/share/fonts/Monaspace/ that start with "Monaspace"
+find "${MONASPACE_LOCATON}" -name "Monaspace*" -delete
+
+# copy all fonts from ./otf to ~/.local/share/fonts
+rsync -a "${REPO_LOCATION}/fonts/otf/"* "${MONASPACE_LOCATON}"
+
+# copy variable fonts from ./variable to ~/.local/share/fonts
+rsync -a "${REPO_LOCATION}/fonts/variable/" "${MONASPACE_LOCATON}"
+
+# on linux, build font information caches
+if [ "$(uname)" == "Linux" ]; then
+    fc-cache -f
+fi
+
+NUM_FONTS=$(find "${MONASPACE_LOCATON}" -name "Monaspace*" | wc -l)
+echo "${NUM_FONTS} fonts installed to '${MONASPACE_LOCATON}'."
